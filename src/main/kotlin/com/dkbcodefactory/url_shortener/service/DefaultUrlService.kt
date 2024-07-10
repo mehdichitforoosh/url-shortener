ca52d02bfad60dd5a18ddc8567e9080c3b3f2349
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2024-07-10 16:36:07 +0200

    Add logger to the service

--- src/main/kotlin/com/dkbcodefactory/url_shortener/service/DefaultUrlService.kt
+++ src/main/kotlin/com/dkbcodefactory/url_shortener/service/DefaultUrlService.kt
@@ -2,6 +2,7 @@ package com.dkbcodefactory.url_shortener.service
 
 import com.dkbcodefactory.url_shortener.model.Url
 import com.dkbcodefactory.url_shortener.repository.UrlRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -10,7 +11,10 @@ class DefaultUrlService @Autowired constructor(
     private val urlRepository: UrlRepository, private val shortUrlGenerator: ShortUrlGenerator
 ) : UrlService {
 
+    private val logger = LoggerFactory.getLogger(DefaultUrlService::class.java)
+
     override fun shortenUrl(originalUrl: String): Url {
+        logger.debug("Shorten url {}", originalUrl)
         var shortUrl = shortUrlGenerator.generate(originalUrl)
         while (urlRepository.hasCollision(shortUrl)) {
             shortUrl = shortUrlGenerator.generate(originalUrl)
@@ -21,6 +25,7 @@ class DefaultUrlService @Autowired constructor(
     }
 
     override fun getOriginalUrl(shortUrl: String): Url? {
+        logger.debug("Get original url for {}", shortUrl)
         return urlRepository.findOriginalUrl(shortUrl)
     }
 
