3f78f4059707237cdb79e9e7ba2a3471ea8a1d5d
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2024-07-10 16:37:17 +0200

    Modify Url controller

--- src/main/kotlin/com/dkbcodefactory/url_shortener/controller/UrlController.kt
+++ src/main/kotlin/com/dkbcodefactory/url_shortener/controller/UrlController.kt
@@ -1,8 +1,11 @@
 package com.dkbcodefactory.url_shortener.controller
 
+import com.dkbcodefactory.url_shortener.controller.json.CreateShortUrlJson
 import com.dkbcodefactory.url_shortener.model.Url
 import com.dkbcodefactory.url_shortener.service.UrlService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -10,12 +13,17 @@ import org.springframework.web.bind.annotation.*
 class UrlController @Autowired constructor(private val urlService: UrlService) {
 
     @PostMapping("/shorten")
-    fun shortenURL(@RequestBody originalUrl: String): Url {
-        return urlService.shortenUrl(originalUrl)
+    fun shortenURL(@RequestBody createShortUrlJson: CreateShortUrlJson): ResponseEntity<Url> {
+        val url = urlService.shortenUrl(createShortUrlJson.originalUrl)
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(url)
     }
 
     @GetMapping("/{shortUrl}")
-    fun getOriginalUrl(@PathVariable shortUrl: String): Url? {
-        return urlService.getOriginalUrl(shortUrl)
+    fun getOriginalUrl(@PathVariable shortUrl: String): ResponseEntity<Url?> {
+        val url = urlService.getOriginalUrl(shortUrl)
+        return url?.let {
+            ResponseEntity.ok(it)
+        } ?: ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
     }
 }
