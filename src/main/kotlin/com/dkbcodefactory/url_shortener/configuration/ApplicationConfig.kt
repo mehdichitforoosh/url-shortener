2bfeaa3858856e7d98b64e83a7bb7445dea415c1
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2024-07-10 16:36:52 +0200

    Add conditional configuration based on application.conf

--- src/main/kotlin/com/dkbcodefactory/url_shortener/configuration/ApplicationConfig.kt
+++ src/main/kotlin/com/dkbcodefactory/url_shortener/configuration/ApplicationConfig.kt
@@ -1,7 +1,11 @@
 package com.dkbcodefactory.url_shortener.configuration
 
+import com.dkbcodefactory.url_shortener.repository.InMemoryUrlRepository
+import com.dkbcodefactory.url_shortener.repository.UrlRepository
 import com.dkbcodefactory.url_shortener.service.RandomShortUrlGenerator
+import com.dkbcodefactory.url_shortener.service.Sha256ShortUrlGenerator
 import com.dkbcodefactory.url_shortener.service.ShortUrlGenerator
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,7 +13,20 @@ import org.springframework.context.annotation.Configuration
 class ApplicationConfig {
 
     @Bean
-    private fun randomShortUrlGenerator(): ShortUrlGenerator {
+    @ConditionalOnProperty(name = ["services.short-url-generator"], havingValue = "random")
+    fun randomShortUrlGenerator(): ShortUrlGenerator {
         return RandomShortUrlGenerator()
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = ["services.short-url-generator"], havingValue = "sha-256")
+    fun sha256ShortUrlGenerator(): ShortUrlGenerator {
+        return Sha256ShortUrlGenerator()
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = ["repositories.url-repository"], havingValue = "in-memory")
+    fun inMemoryUrlRepository(): UrlRepository {
+        return InMemoryUrlRepository()
     }
 }
