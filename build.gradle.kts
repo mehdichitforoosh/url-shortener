9d6bf10648de699851c0b6efbc6fe8ca88302ec3
Mehdi Chitforoosh <mehdichitforoosh@gmail.com>
2024-07-10 16:37:38 +0200

    Remove unused classes and libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,13 +19,12 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-cache")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("io.mockk:mockk:1.13.11")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
